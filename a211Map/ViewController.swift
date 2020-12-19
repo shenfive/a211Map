651f228a222c22be33abd2fa33768ef8fd47282b
Shen Jun Wu <shenfive@gmail.com>
2020-12-19 10:23:57 +0800

    init

--- a211Map/ViewController.swift
+++ a211Map/ViewController.swift
@@ -6,9 +6,11 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myMap: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
