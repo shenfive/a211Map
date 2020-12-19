d5c6c9c701b417e59fc1f12c4e97c68de7062c39
Shen Jun Wu <shenfive@gmail.com>
2020-12-19 11:31:11 +0800

    auth

--- a211Map/ViewController.swift
+++ a211Map/ViewController.swift
@@ -7,37 +7,18 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    var locationManager:CLLocationManager? = nil
+    
     @IBOutlet weak var myMap: MKMapView!
+    
     override func viewDidLoad() {
-        
-      
         super.viewDidLoad()
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-            let latitude:CLLocationDegrees = 25.0446727
-            let longitude:CLLocationDegrees = 121.5127383
-            let location:CLLocationCoordinate2D =
-CLLocationCoordinate2DMake(latitude, longitude)
-            let xScale:CLLocationDegrees = 0.01
-            let yScale:CLLocationDegrees = 0.01
-            let span:MKCoordinateSpan =
-MKCoordinateSpan(latitudeDelta: yScale, longitudeDelta: xScale)
-            let region:MKCoordinateRegion =
-MKCoordinateRegion.init(center: location, span: span)
-            self.myMap.setRegion(region, animated: true)
-            
-            let annotation = MKPointAnnotation()
-            annotation.coordinate = location
-            annotation.title = "譯智"
-            annotation.subtitle = "教育訓練中心"
-            self.myMap.addAnnotation(annotation)
-
-            
-            
-            
-        }
+        locationManager = CLLocationManager()
+        locationManager?.requestWhenInUseAuthorization()
 
     }
 
